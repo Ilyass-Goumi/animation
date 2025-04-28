468cb849934d576e08aa20ea7cf57304c3329738
ilyass <ilyassgoumi11@gmail.com>
2025-04-28 13:40:27 +0200

    deploy

---
+++ app.js
@@ -0,0 +1,46 @@
+/*const buttons = document.querySelectorAll(".filter-btn");
+const menuItems = document.querySelectorAll(".menu-item");
+
+buttons.forEach((button) => {
+   button.addEventListener( 'click', () => {
+    const category = button.getAttribute("data-category") ;
+    
+    menuItems.forEach((item) => {
+        if(category === "all" || category === item.getAttribute("data-category")){
+            item.style.display = "flex";
+          
+        }else{
+            item.style.display = "none";
+        }
+      
+        
+    })
+     
+
+
+   })
+});*/
+
+/*buttons.forEach((button) =>{
+button.addEventListener('click' , () => {
+   const category = button.getAttribute("data-category");
+
+   menuItems.forEach((list1) => {
+     if (category === "all" || category === list1.getAttribute("data-category")){
+        list1.style.display = "flex";
+     }else{
+        list1.style.display = "move";
+     }
+
+   })
+})
+
+})*/
+
+/*const buttons = document.querySelectorAll(".filter-btn");
+const menuItems = document.querySelectorAll(".menu-item");
+
+buttons.forEach((button) => {
+    console.log(button);
+    
+});*/
